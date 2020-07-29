0ec572946714d07da896e8a91335c0db0b6169d6
Robert <1robertchambers@gmail.com>
2020-07-29 22:58:33 +0100

    fixed test, gitignore

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from realtrends import TrendsFetcher
-import time
 import pandas
 
 test_fetcher = TrendsFetcher()
 
 with pandas.option_context("display.max_rows", None, "display.max_columns", None):
-    for tr in ["1-d"]:
-        test_fetcher.scrape_trend(["snow", "ice", "sleet", "hail", "rain"], geo = "GB", time_range = tr, tz = "-60")
+    for tr in ["1-H","4-H","1-d","7-d","1-m","3-m","12-m","5-y"]:
+        test_fetcher.scrape_trend(["snow", "ice", "sleet", "hail", "rain"], geo="GB", time_range=tr, tz="-60")
         print(test_fetcher.trends_data)
 
 
