ae9a0563769010aa190bb469066a1a3d6867cc14
Robert <1robertchambers@gmail.com>
2020-08-01 23:29:24 +0100

    absolute volumes

--- realtrends/real_fetch.py
+++ realtrends/real_fetch.py
@@ -2,12 +2,13 @@ from realtrends import TrendsFetcher
 
 class RealTrendsFetcher:
     # todo find a low volume keyword automatically - scrape wikipedia list of villages?
-    def __get_low_vol_keyword(self):
-        self.__low_volume = "brierly"
+    def __get_ladder(self):
+        self.__ladder = ["brierly", "cinderford", "gloucester"]
+        #self.__low_volume = "brierly"
 
     def __get_reference_index(self):
         relative_fetcher = TrendsFetcher()
-        relative_fetcher.scrape_trend([self.__low_volume],
+        relative_fetcher.scrape_trend([self.__ladder[0]],
                 geo=self.__geo, time_range=self.__time_range, tz=self.__tz)
 
         low_volume_profile = relative_fetcher.trends_data
@@ -15,17 +16,27 @@ class RealTrendsFetcher:
 
         print(low_volume_profile)
 
-        ref_index = low_volume_profile.index[low_volume_profile["brierly"] == 100]
+        self.__ref_index_low = low_volume_profile.index[low_volume_profile[self.__ladder[0]] == 100]
 
-        print(int(low_volume_profile.loc[ref_index[0], "brierly"]))
+        #print(int(low_volume_profile.loc[ref_index[0], "brierly"]))
 
     # todo use some form of means/clustering to infer absolute value?
     def __get_reference_abs(self):
         self.__low_volume_abs = 1
 
-    def __get_low(self):
+    def __get_low_reference(self):
         self.__get_reference_index()
         self.__get_reference_abs()
+
+    def __similar_vol(self, term1, term2):
+        return False
+
+    def __step_absolute(self, low, high):
+        step_data = TrendsFetcher()
+
+        step_data.scrape_trend([low, high],
+                geo=self.__geo, time_range=self.__time_range, tz=self.__tz)
+        print(step_data.trends_data)
 
     def scrape_real(self, keyword, geo="", time_range="1-H",
                     tz="0", save_file="", retry=True):
@@ -34,9 +45,21 @@ class RealTrendsFetcher:
         self.__time_range = time_range
         self.__tz = tz
 
-        self.__get_low_vol_keyword()
+        self.__get_ladder()
 
-        self.__get_low()
+        self.__get_low_reference()
+
+        # work up the ladder until the lowest term is similar in magnitude
+        i = 0
+        comp1 = self.__ladder[i]
+        while self.__similar_vol(self.__keyword, comp1) == False and i < len(self.__ladder)-1:
+            i += 1
+            comp1 = self.__ladder[i-1]
+            comp2 = self.__ladder[i]
+            print("here:")
+            print(comp1)
+            print(comp2)
+            self.__step_absolute(comp1, comp2)
 
     def __init__(self):
         self.__keyword = ""
@@ -44,10 +67,9 @@ class RealTrendsFetcher:
         self.__time_range = ""
         self.__tz = ""
 
-        # keyword to infer absolute values from and start bootstrapping
-        self.__low_volume = ""
         # set of values increasing exponentially in popularity
+        # ladder[0] has absolute values inferred
         self.__ladder = []
         # index in low volume data to use as reference point
-        self.__low_volume_reference_index = ""
-        self.__low_volume_abs = 0
+        self.__ref_index_low = ""
+        self.__abs_vol_low = 0
